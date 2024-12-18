6158a035cadbd03227ba4c4b4ac3dee1b06a8b88
mirrorbots <itstrytemp112299@gmail.com>
2024-12-18 14:38:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "25121213")
     API_HASH = environ.get("API_HASH", "b734dcc45da130a8156e2be836594706")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7620239040:AAHEQMTn3zvcCEm2dhofq-eNp3MLfpGYexU") 
-    BOT_SESSION = environ.get("BOT_SESSION", "Pragyan") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://AvyukthX:AvyukthX@bot.ctdiudr.mongodb.net/?retryWrites=true&w=majority")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "PragyanForwardBot")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7483848628:AAG65IBntV1N7MSCHJBgDVwjpHieYT-i6lQ") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Hope") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://meto:znhgeOjRiB7jHEcq@for.ajot6.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "for")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6469067345').split()]
 
 class temp(object): 
