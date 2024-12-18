32cb424a49a3233ec0a601f95f3516741b4e596e
mirrorbots <itstrytemp112299@gmail.com>
2024-12-18 14:13:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = environ.get("API_ID", "25121213")
     API_HASH = environ.get("API_HASH", "b734dcc45da130a8156e2be836594706")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7620239040:AAHEQMTn3zvcCEm2dhofq-eNp3MLfpGYexU") 
-    BOT_SESSION = environ.get("BOT_SESSION", "Hope") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Pragyan") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://forward:4OdmXJGM0HDXqFTN@forwarder.pi1ea.mongodb.net/?retryWrites=true&w=majority")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "ForwardBot")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "PragyanForwardBot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6469067345').split()]
 
 class temp(object): 
@@ -15,4 +15,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
