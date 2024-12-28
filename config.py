7bc8209020934d69ae6f01ff56520387554df256
mirrorbots <itstrytemp112299@gmail.com>
2024-12-28 17:51:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "b734dcc45da130a8156e2be836594706")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7483848628:AAG65IBntV1N7MSCHJBgDVwjpHieYT-i6lQ") 
     BOT_SESSION = environ.get("BOT_SESSION", "Hope") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://meto:znhgeOjRiB7jHEcq@for.ajot6.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://for1:dontcp@cluster0.cy6zb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "for")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6469067345').split()]
 
